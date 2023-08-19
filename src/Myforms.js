631c1740b8777283806bff452b69d9edf4058c9d
windows <windows@microsoft.com>
2023-08-19 14:55:57 +0530

    text shadow

---
+++ src/Myforms.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import "./style.css";
+
+function Myforms() {
+  return (
+    <Fragment>
+    <div>Myforms</div>
+    <h1 className='h1'>this is form element</h1>
+    </Fragment>
+  )
+}
+
+export default Myforms
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Abc, { Myhome1 } from './Home';
 import Myclass from './About';
 import Myimage from './Myimage';
+import Myforms from './Myforms';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,6 +13,7 @@ root.render(
     <Abc></Abc>
     <Myhome1></Myhome1>
     <Myimage></Myimage>
+    <Myforms/>
   </React.StrictMode>
 );
 
